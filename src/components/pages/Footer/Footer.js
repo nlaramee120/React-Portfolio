447e9d2ef19324bd0e2efea5ca7330b56e68aced
nlaramee120 <nlaramee120@aol.com>
2021-12-18 00:49:51 -0600

    Created the boilerplate for the main page container as well as the child pages. Created a basic footer to be edited.

---
+++ src/components/pages/Footer/Footer.js
@@ -0,0 +1,11 @@
+import react from 'react';
+import './footer.css'
+
+
+export default function Footer() {
+    return (
+
+        <div className="footer"><p>This is some sticky text</p></div>
+
+    )
+}
---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Nick Laramee</h1>
+      <p> 
+      Hello! My name is Nick Laramee. I just recently graduated with a Bachelor's in Athletic Training but I am now currently looking to switch gears! I am currently enrolled in a coding and web development bootcamp through Northwestern, and am having a lot of fun creating new projects. Take a look at some of my work below!
+      </p>
+    </div>
+  );
+}
