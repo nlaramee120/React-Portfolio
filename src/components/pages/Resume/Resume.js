7a22c160769d562479ee5a627d14c0691d965415
nlaramee120 <nlaramee120@aol.com>
2022-03-31 00:44:21 -0500

    many stylistic changes.

--- src/components/pages/Resume/Resume.js
+++ src/components/pages/Resume/Resume.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import resume from './assets/resume.pdf'
+import resume from './assets/myResume.pdf'
 
 export default function Resume() {
   return (
--- src/components/pages/Work/Work.js
+++ src/components/pages/Work/Work.js
@@ -9,35 +9,57 @@ export default function Work() {
   return (
 
 
+    <div className = 'flex-container'>
 
-    <div className="container">
+        <div class='item'>
+          <a href="https://nlaramee120.github.io/day-planner/" target="_blank">
+            <img className="work-img" alt='work-scheduler' src={Work1} /></a>Work Scheduler<p>A simple app that uses local storage to help plan your day</p></div>
+        <div class='item'>
+          <a href="https://nlaramee120.github.io/Password-Generator/" target="_blank">
+          <img className="work-img" alt='password-generator' src={Work2} /></a>Password Generator
+        </div>
+        <div class='break'></div>
+        <div class='item'>
+        <a href="https://nlaramee120.github.io/Make-My-Day/" target="_blank">
+        <img className="work-img" alt='make-my-day' src={Work3} /></a>Make My Day
+        </div>
+        <div class='item'>
+          <a href="https://sheltered-sierra-22318.herokuapp.com/" target="_blank">
+           <img className="work-img" alt='cloudsource' width="1000" height="200" src={Work4} /></a>CloudSource
+        </div>
 
-      <h1>Work</h1>
-    
-    <a href="https://nlaramee120.github.io/day-planner/" target="_blank">
-      <div class="gallery gallery__item--1">
-        <img className="work-img" alt='work-scheduler' width="600" height="400" src={Work1} />
-        <div className="desc">Work Scheduler</div>
-      </div>
-      </a>
-      <a href="https://nlaramee120.github.io/Password-Generator/" target="_blank">
-      <div class="gallery gallery__item--2">
-        <img className="work-img" alt='password-generator' width="600" height="400" src={Work2} />
-        <div className="desc">Password Generator</div>
-      </div>
-      </a>
-      <a href="https://nlaramee120.github.io/Make-My-Day/" target="_blank">
-      <div class="gallery gallery__item--3">
-        <img className="work-img" alt='make-my-day' width="600" height="400" src={Work3} />
-        <div className="desc">Make My Day</div>
-      </div>
-      </a>
-      <a href="https://sheltered-sierra-22318.herokuapp.com/" target="_blank">
-      <div class="gallery gallery__item--4">
-        <img className="work-img" alt='cloudsource' width="1000" height="200" src={Work4} />
-        <div className="desc">Cloudsource</div>
-      </div>
-      </a>
+
+
     </div>
+
+    // <div className="container">
+
+    //   <h1>Work</h1>
+    
+    // <a href="https://nlaramee120.github.io/day-planner/" target="_blank">
+    //   <div class="gallery gallery__item--1">
+    //     <img className="work-img" alt='work-scheduler' width="600" height="400" src={Work1} />
+    //     <div className="desc">Work Scheduler</div>
+    //   </div>
+    //   </a>
+    //   <a href="https://nlaramee120.github.io/Password-Generator/" target="_blank">
+    //   <div class="gallery gallery__item--2">
+    //     <img className="work-img" alt='password-generator' width="600" height="400" src={Work2} />
+    //     <div className="desc">Password Generator</div>
+    //   </div>
+    //   </a>
+    //   <a href="https://nlaramee120.github.io/Make-My-Day/" target="_blank">
+    //   <div class="gallery gallery__item--3">
+    //     <img className="work-img" alt='make-my-day' width="600" height="400" src={Work3} />
+    //     <div className="desc">Make My Day</div>
+    //   </div>
+    //   </a>
+    //   <a href="https://sheltered-sierra-22318.herokuapp.com/" target="_blank">
+    //   <div class="gallery gallery__item--4">
+    //     <img className="work-img" alt='cloudsource' width="1000" height="200" src={Work4} />
+    //     <div className="desc">Cloudsource</div>
+    //   </div>
+    //   </a>
+    // </div>
   );
 }
